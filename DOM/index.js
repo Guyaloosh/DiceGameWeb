8980b683df8d54a2d053650e2e44c743b9266d9e
Guyaloosh <guyaloosh@gmail.com>
2023-07-22 07:20:13 +0300

    making the dice to roll every click , fix some of the styles and add a favicon to the page .

--- DOM/index.js
+++ DOM/index.js
@@ -1,25 +1,37 @@
 // index.js
 function rollDice() {
-  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-  var randomDiceImage1 = "dice" + randomNumber1 + ".png";
-  var randomImageSource1 = "images/" + randomDiceImage1;
+    var rollCount = 0;
+    var maxRolls = 8; // Change this value to control the number of rolls before showing the result
+    var intervalTime = 100; // Time in milliseconds between each dice change (0.1 seconds)
+    //intervalfunction is a function that call herself until she get to the max . here i use that every 0.1 sec its calling her self 
+    //and after 8 times its give me the results 
+    var interval = setInterval(function () {
+        var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+        var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+        var randomImageSource1 = "images/" + randomDiceImage1;
 
-  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-  var randomDiceImage2 = "dice" + randomNumber2 + ".png";
-  var randomImageSource2 = "images/" + randomDiceImage2;
+        var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+        var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+        var randomImageSource2 = "images/" + randomDiceImage2;
 
-  // Update the image sources
-  document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
-  document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+        // Update the image sources
+        document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
+        document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
-  // Update the winner message
-  if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player Number 1 Wins! 🚩";
-  } else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").innerHTML = "Player Number 2 Wins! 🚩";
-  } else {
-    document.querySelector("h1").innerHTML = "It's a Draw! 🚩";
-  }
+        rollCount++;
+        if (rollCount === maxRolls) {
+            clearInterval(interval);
+
+            // Update the winner message
+            if (randomNumber1 > randomNumber2) {
+                document.querySelector("h1").innerHTML = "Player Number 1 Wins! 🚩";
+            } else if (randomNumber2 > randomNumber1) {
+                document.querySelector("h1").innerHTML = "Player Number 2 Wins! 🚩";
+            } else {
+                document.querySelector("h1").innerHTML = "It's a Draw! 🚩";
+            }
+        }
+    }, intervalTime);
 }
 
 document.getElementById("rollButton").addEventListener("click", rollDice);
